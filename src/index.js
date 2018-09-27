5f8319d23bf4730ce8c533a308b5f91bda3026ec
karantikku <16414052+karantikku@users.noreply.github.com>
2018-09-27 14:50:41 +0530

    form 1

--- src/index.js
+++ src/index.js
@@ -5,47 +5,36 @@ import "./styles.css";
 const app = {
   title: "Indecision App",
   subtitle: "Put your life in the hands of Computer",
-  options: ["One", "Two"]
+  options: []
 };
-const template = (
-  <div>
-    {app.title && <h1>{app.title}</h1>}
-    {app.subtitle && <p>{app.subtitle}</p>}
-    {app.options ? "Here are your options" : "No Options"}
-    <ol>
-      <li>{app.title}</li>
-      <li>{app.subtitle}</li>
-    </ol>
-  </div>
-);
-let count = 0;
-const addOne = () => {
-  console.log("addOne");
-  count++;
-  renderCounterApp();
-};
-const minusOne = () => {
-  console.log("minusOne");
-  count--;
-  renderCounterApp();
-};
-const reset = () => {
-  console.log("reset");
-  count = 0;
-  renderCounterApp();
+const onFormSubmit = e => {
+  e.preventDefault();
+  const option = e.target.elements.options.value;
+  if (option) {
+    app.options.push(option);
+    e.target.elements.options.value = "";
+    render();
+  }
 };
 
 const appRoot = document.getElementById("root");
-
-const renderCounterApp = () => {
-  const templateTwo = (
+const render = () => {
+  const template = (
     <div>
-      <h1>Count: {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>reset</button>
+      {app.title && <h1>{app.title}</h1>}
+      {app.subtitle && <p>{app.subtitle}</p>}
+      {app.options.length ? "Here are your options" : "No Options"}
+      <p>{app.options.length}</p>
+      <ol>
+        <li>{app.title}</li>
+        <li>{app.subtitle}</li>
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="options" />
+        <button>Add Option</button>
+      </form>
     </div>
   );
-  ReactDOM.render(templateTwo, appRoot);
+  ReactDOM.render(template, appRoot);
 };
-renderCounterApp();
+render();
