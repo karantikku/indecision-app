6d82a725bebfe9909588980d25a26fe647107749
karantikku <16414052+karantikku@users.noreply.github.com>
2018-09-28 22:29:53 +0530

    sec 4  33/180, state concept

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,5 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles.css";
 
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleAddOne = this.handleAddOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.state = {
+      count: 0
+    };
+  }
+  handleMinusOne() {
+    this.setState(prevState => {
+      return {
+        count: prevState.count - 1
+      };
+    });
+  }
+  handleAddOne() {
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      };
+    });
+  }
+  handleReset() {
+    this.setState(() => {
+      return {
+        count: 0
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1> Count: {this.state.count}</h1>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>reset</button>
+      </div>
+    );
+  }
+}
 const appRoot = document.getElementById("root");
+ReactDOM.render(<Counter />, appRoot);
